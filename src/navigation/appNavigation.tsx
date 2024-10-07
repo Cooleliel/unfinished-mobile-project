65e2400b471e9a8c27a54fbcacc50600ed82eff5
cooleliel <elielcoulibaly39@gmail.com>
2024-10-07 15:27:36 +0000

    concevoir check screen

--- src/navigation/appNavigation.tsx
+++ src/navigation/appNavigation.tsx
@@ -3,14 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUpScreen from '../screens/signup/SignUpScreen';
 import RemovePasswordScreen from '../screens/removepassword/RemovePasswordScreen';
+import CheckScreen from '../screens/check/CheckScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='RemovePassword' screenOptions={{headerShown: false}}>
-                <Stack.Screen name='RemovePassword' component={RemovePasswordScreen}/>
+            <Stack.Navigator initialRouteName='Check' screenOptions={{headerShown: false}}>
+                <Stack.Screen name='Check' component={CheckScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
